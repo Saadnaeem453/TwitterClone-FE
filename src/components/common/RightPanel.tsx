c56cb382149ced16e484fcfaeda1e61bdc775b38
Saadnaeem453 <jsaad3663@gmail.com>
2024-08-29 10:52:35 +0500

    Complete  auth

--- src/components/common/RightPanel.tsx
+++ src/components/common/RightPanel.tsx
@@ -2,64 +2,81 @@
 import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
 import Link from "next/link";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import Image from "next/image"
+import Image from "next/image";
 import { useGetMe } from "@/hooks/useGetMe";
+
 const RightPanel = () => {
 	const { data: authUser, isLoading } = useGetMe();
 
-if(!authUser){
-	return null
-}
+	console.log("authUser", authUser);
+
+	if (isLoading) {
+		return (
+			<div className='hidden lg:block my-4 mx-2'>
+				<div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
+					<p className='font-bold'>Who to follow</p>
+					<div className='flex flex-col gap-4'>
+						<RightPanelSkeleton />
+						<RightPanelSkeleton />
+						<RightPanelSkeleton />
+						<RightPanelSkeleton />
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	if (!authUser) {
+		return null;
+	}
+
 	return (
-		<>
 		<div className='hidden lg:block my-4 mx-2'>
 			<div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
 				<p className='font-bold'>Who to follow</p>
 				<div className='flex flex-col gap-4'>
-					{/* item */}
-					{isLoading && (
-						<>
-							<RightPanelSkeleton />
-							<RightPanelSkeleton />
-							<RightPanelSkeleton />
-							<RightPanelSkeleton />
-						</>
+					{USERS_FOR_RIGHT_PANEL && USERS_FOR_RIGHT_PANEL.length > 0 ? 
+					(USERS_FOR_RIGHT_PANEL?.map((user) => (
+						<Link
+							href={`/profile/${user.username}`}
+							className='flex items-center justify-between gap-4'
+							key={user._id}
+						>
+							<div className='flex gap-2 items-center'>
+								<div className='avatar'>
+									<div className='w-8 rounded-full'>
+										<Image
+											width={32}
+											height={32}
+											alt="profileimg"
+											src={user.profileImg || "/avatar-placeholder.png"}
+										/>
+									</div>
+								</div>
+								<div className='flex flex-col'>
+									<span className='font-semibold tracking-tight truncate w-28'>
+										{user.fullName}
+									</span>
+									<span className='text-sm text-slate-500'>@{user.username}</span>
+								</div>
+							</div>
+							<div>
+								<button
+									className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
+									onClick={(e) => e.preventDefault()}
+								>
+									Follow
+								</button>
+							</div>
+						</Link>
+					))) : (
+						
+						<p className='text-center text-slate-500'>No suggestions available.</p>
 					)}
-					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
-							<Link
-								href={`/profile/${user.username}`}
-								className='flex items-center justify-between gap-4'
-								key={user._id}
-							>
-								<div className='flex gap-2 items-center'>
-									<div className='avatar'>
-										<div className='w-8 rounded-full'>
-											<Image width={32} height={32} alt="profileimg" src={user.profileImg || "/avatar-placeholder.png"} />
-										</div>
-									</div>
-									<div className='flex flex-col'>
-										<span className='font-semibold tracking-tight truncate w-28'>
-											{user.fullName}
-										</span>
-										<span className='text-sm text-slate-500'>@{user.username}</span>
-									</div>
-								</div>
-								<div>
-									<button
-										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
-									>
-										Follow
-									</button>
-								</div>
-							</Link>
-						))}
 				</div>
 			</div>
-		</div> 
-	
-		</>
+		</div>
 	);
 };
+
 export default RightPanel;
--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -8,90 +8,82 @@ import Link from "next/link";
 import Image from "next/image";
 import { useLogOutMutation } from "@/hooks/useLogOutMutation";
 import { useGetMe } from "@/hooks/useGetMe";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Sidebar = () => {
-	const { data: authUser, isLoading } = useGetMe();
-	const{mutate:logout, error, isError, isPending} = useLogOutMutation();
-	const data = {
-		fullName: "John Doe ",
-		username: "johndoe",
-		profileImg: "/avatars/boy1.png",
-	};
-	if(!authUser){
-		return null
-	}
-	
-	return (
-		<>
-		
-		<div className='md:flex-[2_2_0] w-18 max-w-52'>
-			<div className='sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full'>
-				<Link href='/' className='flex justify-center md:justify-start'>
-					<XSvg className='px-2 w-12 h-12 rounded-full fill-white hover:bg-stone-900' />
-				</Link>
-				<ul className='flex flex-col gap-3 mt-4'>
-					<li className='flex justify-center md:justify-start'>
-						<Link
-							href='/'
-							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
-						>
-							<MdHomeFilled className='w-7 h-7' />
-							<span className='text-lg hidden md:block'>Home</span>
-						</Link>
-					</li>
-					<li className='flex justify-center md:justify-start'>
-						<Link
-							href='/notifications'
-							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
-						>
-							<IoNotifications className='w-6 h-6' />
-							<span className='text-lg hidden md:block'>Notifications</span>
-						</Link>
-					</li>
+  const { data: authUser, isLoading } = useGetMe();
+  const { mutate: logout, error, isError, isPending } = useLogOutMutation();
 
-					<li className='flex justify-center md:justify-start'>
-						<Link
-							href={`/profile/${data?.username}`}
-							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
-						>
-							<FaUser className='w-6 h-6' />
-							<span className='text-lg hidden md:block'>Profile</span>
-						</Link>
-					</li>
-				</ul>
-				{data && (
-					<Link
-						href={`/profile/${data.username}`}
-						className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
-					>
-						<div className='avatar hidden md:inline-flex'>
-							<div className='w-8 rounded-full'>
-								<Image src={data?.profileImg || "/avatar-placeholder.png"} alt="profileImage" width={20} height={20} />
-							</div>
-						</div>
-						<div className='flex justify-between flex-1'>
-							<div className='hidden md:block'>
-								<p className='text-white font-bold text-sm w-20 truncate'>{data?.fullName}</p>
-								<p className='text-slate-500 text-sm'>@{data?.username}</p>
-							</div>
-							
-							<BiLogOut className='w-5 h-5 cursor-pointer' 
-						onClick={(e)=>
-						{
-							e.preventDefault()
-							logout();
-						}
-							
-							}
-										/>
-						</div>
-					</Link>
-						
+  return (
+    <div className='md:flex-[2_2_0] w-18 max-w-52'>
+      <div className='sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full'>
+        <Link href='/' className='flex justify-center md:justify-start'>
+          <XSvg className='px-2 w-12 h-12 rounded-full fill-white hover:bg-stone-900' />
+        </Link>
+        <ul className='flex flex-col gap-3 mt-4'>
+          <li className='flex justify-center md:justify-start'>
+            <Link
+              href='/'
+              className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
+            >
+              <MdHomeFilled className='w-7 h-7' />
+              <span className='text-lg hidden md:block'>Home</span>
+            </Link>
+          </li>
+          <li className='flex justify-center md:justify-start'>
+            <Link
+              href='/notifications'
+              className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
+            >
+              <IoNotifications className='w-6 h-6' />
+              <span className='text-lg hidden md:block'>Notifications</span>
+            </Link>
+          </li>
 
-				)}
-			</div>
-		</div>
-		</>
-	);
+          <li className='flex justify-center md:justify-start'>
+            <Link
+              href={`/profile/${authUser?.username}`}
+              className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
+            >
+              <FaUser className='w-6 h-6' />
+              <span className='text-lg hidden md:block'>Profile</span>
+            </Link>
+          </li>
+        </ul>
+
+        {isLoading ? (
+          // Show a loading spinner in place of the profile section while loading
+          <div className='mt-auto mb-10 flex justify-center items-center'>
+            <LoadingSpinner size='lg' />
+          </div>
+        ) : authUser && (
+          // Show the user profile section once the data is loaded
+          <Link
+            href={`/profile/${authUser.username}`}
+            className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
+          >
+            <div className='avatar hidden md:inline-flex'>
+              <div className='w-8 rounded-full'>
+                <Image src={authUser?.profileImg || "/avatar-placeholder.png"} alt="profileImage" width={32} height={32} />
+              </div>
+            </div>
+            <div className='flex justify-between flex-1'>
+              <div className='hidden md:block'>
+                <p className='text-white font-bold text-sm w-20 truncate'>{authUser?.fullname}</p>
+                <p className='text-slate-500 text-sm'>@{authUser?.username}</p>
+              </div>
+              <BiLogOut className='w-5 h-5 cursor-pointer' 
+                onClick={(e) => {
+                  e.preventDefault();
+                  logout();
+                }}
+              />
+            </div>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
+
 export default Sidebar;
